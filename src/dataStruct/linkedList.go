d3c4a31231113f7985dfe217386a31b1ff5d792f
tyrl76 <tyrl76@gmail.com>
2021-09-05 01:16:13 +0900

    21/09/05 ~28강 Packaging과 Stack, Queue

---
+++ src/dataStruct/linkedList.go
@@ -0,0 +1,97 @@
+package dataStruct
+
+import "fmt"
+
+type Node struct {
+	Next *Node
+	Prev *Node
+	Val  int
+}
+
+type LinkedList struct {
+	Root *Node
+	Tail *Node
+}
+
+func (l *LinkedList) AddNode(Val int) {
+	if l.Root == nil {
+		l.Root = &Node{Val: Val}
+		l.Tail = l.Root
+		return
+	}
+	l.Tail.Next = &Node{Val: Val}
+	Prev := l.Tail
+	l.Tail = l.Tail.Next
+	l.Tail.Prev = Prev
+}
+
+func (l *LinkedList) Back() int {
+	if l.Tail != nil {
+		return l.Tail.Val
+	}
+	return 0
+}
+
+func (l *LinkedList) Front() int {
+	if l.Root != nil {
+		return l.Root.Val
+	}
+	return 0
+}
+
+func (l *LinkedList) Empty() bool {
+	return l.Root == nil
+}
+
+func (l *LinkedList) PopBack() {
+	if l.Tail == nil {
+		return
+	}
+	l.RemoveNode(l.Tail)
+}
+
+func (l *LinkedList) PopFront() {
+	if l.Root == nil {
+		return
+	}
+	l.RemoveNode(l.Root)
+}
+
+func (l *LinkedList) RemoveNode(node *Node) {
+	if node == l.Root {
+		l.Root = l.Root.Next
+		if l.Root != nil {
+			l.Root.Prev = nil
+		}
+		node.Next = nil
+		return
+	}
+
+	Prev := node.Prev
+
+	if node == l.Tail {
+		Prev.Next = nil
+		l.Tail.Prev = nil
+		l.Tail = Prev
+	} else {
+		Prev.Next = Prev.Next.Next
+	}
+}
+
+func (l *LinkedList) PrintNodes() {
+	node := l.Root
+	for node.Next != nil {
+		fmt.Printf("%d -> ", node.Val)
+		node = node.Next
+	}
+	fmt.Printf("%d\n", node.Val)
+}
+
+func (l *LinkedList) PrintReverse() {
+	node := l.Tail
+	for node.Prev != nil {
+		fmt.Printf("%d -> ", node.Val)
+		node = node.Prev
+	}
+	fmt.Printf("%d\n", node.Val)
+}
